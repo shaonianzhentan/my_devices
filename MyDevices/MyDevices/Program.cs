8b73903feb697e0717fb6fafb16ef3aa3f2f9421
zhulu <zhululu@weihaizixun.com>
2021-07-23 11:20:27 +0800

    初始化项目

---
+++ MyDevices/MyDevices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDevices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
